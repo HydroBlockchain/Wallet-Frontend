56d06d127ae531a202fe7740f0a2ccbad86cf6fc
Njoku Emmanuel <kalunjoku123@gmail.com>
2020-09-12 16:39:02 +0100

    implementing animation function🔥🎨

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import SplashScreen from "react-native-splash-screen";
-import { StyleSheet, Text, View, StatusBar, Platform } from "react-native";
+import { Animated, Easing, View, StatusBar, Platform } from "react-native";
 import AppContainer from "./navigation/AppContainer";
+import LottieView from "lottie-react-native";
 import ThemeContextProvider from "./hooks/useTheme";
 
 const App = () => {
+  const [animationProgress, setAnimationProgress] = useState({
+    progress: new Animated.Value(0),
+  });
   useEffect(() => {
     SplashScreen.hide();
   }, []);
+  if (!showWave) {
+    return (
+      <View
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <LottieView
+          source={require("./assets/waves.json")}
+          autoPlay
+          loop
+          style={{
+            height: "100%",
+            width: "100%",
+          }}
+        />
+      </View>
+    );
+  }
   return (
     <ThemeContextProvider>
       <AppContainer />
