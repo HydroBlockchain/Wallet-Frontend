973f0a59052546f72fc4c0620bdfc66e363f401d
Njoku Emmanuel <Njoku Emmanuel>
2020-10-05 12:09:58 +0100

    Implement address types

---
+++ src/screens/context/types.js
@@ -0,0 +1,7 @@
+// address type
+export const GET_IDENTITY_ADDRESS = 'GET_IDENTITY_ADDRESS'
+export const GET_HYDRO_ADDRESS = 'GET_HYDRO_ADDRESS'
+
+//error
+export const ADDRESS_ERROR = 'ADDRESS_ERROR'
+export const CLEAR_ERRORS = 'CLEAR_ERRORS'
